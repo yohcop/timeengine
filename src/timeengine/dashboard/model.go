2c83a098fc575ef8224ae1cdb548cbeee8c6c0ef
Yohann Coppel <yc@leppoc.net>
2013-09-02 16:13:30 -0700

    parameterized dashboards config

--- src/timeengine/dashboard/model.go
+++ src/timeengine/dashboard/model.go
@@ -21,9 +21,9 @@ type DashConfig struct {
 }
 
 type Graph struct {
-	Name       string
-	Targets    []string
-	Resolution int64
+	Name              string
+	Targets           []string
+	Resolution        int64
 }
 
 func (d *Dashboard) Cfg() (*DashConfig, error) {
@@ -35,15 +35,15 @@ func (d *Dashboard) Cfg() (*DashConfig, error) {
 func (d *Dashboard) IsAcled(email string) (bool, error) {
 	cfg, err := d.Cfg()
 	if err != nil {
-    // If there is an error reading from the DB, we ignore.
-    // This is useful in when updates break the schema.
-    // If this never happen, we can do:
+		// If there is an error reading from the DB, we ignore.
+		// This is useful in when updates break the schema.
+		// If this never happen, we can do:
 		// return false, err
 	}
-  // If there is no one in the ACL list, this is considered public.
-  if len(cfg.ACL) == 0 {
-    return true, nil
-  }
+	// If there is no one in the ACL list, this is considered public.
+	if len(cfg.ACL) == 0 {
+		return true, nil
+	}
 	for _, authorizedEmail := range cfg.ACL {
 		if authorizedEmail == email {
 			return true, nil
