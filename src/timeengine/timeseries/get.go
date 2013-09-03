155fd7d6677a30a35f16da38d9841c192bbf4063
Yohann Coppel <yc@leppoc.net>
2013-09-02 20:25:10 -0700

    request data from datastore in parallel

--- src/timeengine/timeseries/get.go
+++ src/timeengine/timeseries/get.go
@@ -1,7 +1,10 @@
 package timeseries
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"sync"
 
 	"timeengine/ae"
 	"timeengine/timeseries/points"
@@ -9,18 +12,56 @@ import (
 
 var _ = log.Println
 
+func encodeSerieDef(def *SerieDef) string {
+	return fmt.Sprintf("%s@%d@%d@%d", def.M, def.R, def.T, def.To)
+}
+
 func GetData(c ae.Context, req *GetReq) (*GetResp, error) {
 	resp := &GetResp{}
+	// TODO: make each call in a separate go routine.
+	defs := make(map[string]*SerieDef)
+	data := make(map[string][]points.StatsDataPoint)
+	// Some series may be duplicated since they may have different
+	// summary functions (serie.S). So find the unique series.
 	for _, serie := range req.Serie {
-		log.Println(serie.S)
-
-		summaryFn := points.GetSummarySelector(serie.S)
-		res := points.SelectFrameSize(serie.R)
-		pts, err := points.GetPoints(c, serie.M,
-			points.NewSpan(res, serie.T, serie.To))
+		k := encodeSerieDef(serie)
+		defs[k] = serie
+		data[k] = nil
+	}
+	// Now request the data for each unique definition.
+	dataMutex := sync.Mutex{}
+	// Go routines put a nil in the done channel if everything went ok
+	// otherwise they put an error.
+	done := make(chan error)
+	for key, serie := range defs {
+		go func(k string, s *SerieDef) {
+			res := points.SelectFrameSize(s.R)
+			pts, err := points.GetPoints(c, s.M,
+				points.NewSpan(res, s.T, s.To))
+			if err != nil {
+				done <- errors.New("Error while getting data: " + err.Error())
+				return
+			}
+			dataMutex.Lock()
+			data[k] = pts
+			dataMutex.Unlock()
+			done <- nil
+		}(key, serie)
+	}
+	// Wait until all the go routines are done.
+	for _ = range defs {
+		err := <-done
 		if err != nil {
 			return nil, err
 		}
+	}
+
+	// Compute the outpug for each requested Serie, using the
+	// data retrieved.
+	for _, serie := range req.Serie {
+		k := encodeSerieDef(serie)
+		res := points.SelectFrameSize(serie.R)
+		pts, _ := data[k]
 		maxPoints := int(serie.To - serie.T)
 		if res > 1 {
 			maxPoints /= int(res)
@@ -29,8 +70,7 @@ func GetData(c ae.Context, req *GetReq) (*GetResp, error) {
 			Target:     serie.M + "@" + serie.S,
 			Datapoints: make([]*DataPoint, 0, maxPoints),
 		}
-		//log.Printf("Got %d points @%d, prepared for %d: %d\n",
-		//    len(pts), int(res), maxPoints, serie.To - serie.T)
+		summaryFn := points.GetSummarySelector(serie.S)
 		for _, p := range pts {
 			t := float64(p.GetTs())
 			v := summaryFn(p)
